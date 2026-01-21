f913dd2dc75e2ec672ee8837fd84b6030f9ba26f
Rohan Jha <168259832+RohanJha17@users.noreply.github.com>
2026-01-21 17:36:04 +0530

    Time: 7 ms (14.74%) | Memory: 125.1 MB (6.98%) - LeetSync

--- LeetCode/334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ LeetCode/334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -5,20 +5,20 @@ public:
 
         if(n < 3) return false;
 
-        vector<int> pre_min(n);
-        pre_min[0] = nums[0];
+        vector<int> p_min(n);
+        p_min[0] = nums[0];
         for(int i=1; i<n; i++){
-            pre_min[i] = min(pre_min[i-1], nums[i]);
+            p_min[i] = min(p_min[i-1], nums[i]);
         }
 
-        vector<int> suf_max(n);
-        suf_max[n-1] = nums[n-1];
+        vector<int> s_max(n);
+        s_max[n-1] = nums[n-1];
         for(int i=n-2; i>=0; i--){
-            suf_max[i] = max(suf_max[i+1], nums[i]);
+            s_max[i] = max(s_max[i+1], nums[i]);
         }
 
         for(int i=1; i<n-1; i++){
-            if(pre_min[i-1] < nums[i] && nums[i] < suf_max[i+1])
+            if(p_min[i-1] < nums[i] && nums[i] < s_max[i])
                 return true;
         }
 
