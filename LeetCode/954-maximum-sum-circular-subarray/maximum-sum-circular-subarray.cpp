fd63082c2d83e81730117830845f06e8b228219b
Rohan Jha <168259832+RohanJha17@users.noreply.github.com>
2026-02-24 11:21:07 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (48.76%) - LeetSync

--- LeetCode/954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
+++ LeetCode/954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
@@ -1,36 +1,20 @@
 class Solution {
 public:
-    int maxSubarraySum(vector<int>& nums){
-        int maxSum = nums[0];
-        int bestEnding = nums[0];
-
-        for(int i=1; i<nums.size(); i++){
-            bestEnding = max(nums[i], bestEnding+nums[i]);
-            maxSum = max(maxSum, bestEnding);
-        }
-        return maxSum;
-    }
-    int minSubarraySum(vector<int>& nums){
-        int bestEnding = nums[0];
-        int minSum = nums[0];
-
-        for(int i=1; i<nums.size(); i++){
-            bestEnding = min(nums[i], bestEnding+nums[i]);
-            minSum = min(minSum, bestEnding);
-        }
-        return minSum;
-    }
     int maxSubarraySumCircular(vector<int>& nums) {
-        int maxSum = maxSubarraySum(nums);
-        int minSum = minSubarraySum(nums);
-
-        int sum = nums[0];
+        int sum = nums[0], maxSum = nums[0], minSum = nums[0];
+        int bestEndingMax = nums[0], bestEndingMin = nums[0];
         for(int i=1; i<nums.size(); i++){
             sum += nums[i];
+
+            bestEndingMax = max(nums[i], nums[i]+bestEndingMax);
+            maxSum = max(maxSum, bestEndingMax);
+
+            bestEndingMin = min(nums[i], nums[i]+bestEndingMin);
+            minSum = min(minSum, bestEndingMin);
         }
         if(sum == minSum)
             return maxSum;
-            
+
         return max(maxSum, sum-minSum);
     }
 };
