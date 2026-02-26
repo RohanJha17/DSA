0212f713810df88234c31b0f6f271e91c551426c
Rohan Jha <168259832+RohanJha17@users.noreply.github.com>
2026-02-26 13:07:31 +0530

    Time: 22 ms (69.89%) | Memory: 88.2 MB (72.59%) - LeetSync

---
+++ LeetCode/525-contiguous-array/contiguous-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        unordered_map<int, int> m;
+        
+        int zero = 0, one = 0, maxLen = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == 0)
+                zero++;
+            else
+                one++;
+
+            int diff = zero - one;
+            if(diff == 0){
+                maxLen = max(maxLen, i+1);
+            }
+
+            if(m.find(diff) == m.end())
+                m[diff] = i;
+            else
+                maxLen = max(maxLen, i-m[diff]);
+        }
+
+        return maxLen;
+    }
+};
