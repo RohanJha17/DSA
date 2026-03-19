075f16cb5286e4dedde6cc041fd79bfbfab073c1
Rohan Jha <168259832+RohanJha17@users.noreply.github.com>
2026-03-19 11:37:05 +0530

    Time: 56 ms (52.22%) | Memory: 72.8 MB (51.64%) - LeetSync

--- LeetCode/1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ LeetCode/1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
+    int distance(vector<int> point){
+        int x = point[0], y = point[1];
+        return x*x + y*y;
+    }
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<int, vector<int>>> pq;
+        priority_queue<pair<int, int>> pq;
+        for(int i=0; i<k; i++){
+            pq.push({distance(points[i]), i});
+        }
 
-        for(auto& p : points){
-            int dist = p[0]*p[0] + p[1]*p[1];
-
-            pq.push({dist, p});
-            if(pq.size() > k)
+        for(int i=k; i<points.size(); i++){
+            int d = distance(points[i]);
+            if(d < pq.top().first){
                 pq.pop();
+                pq.push({d, i});
+            }
         }
 
         vector<vector<int>> ans;
         while(!pq.empty()){
-            ans.push_back(pq.top().second);
+            ans.push_back(points[pq.top().second]);
             pq.pop();
         }
 
